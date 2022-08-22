4119bf2cb0072f7956fda1f089af478c16e0d86b
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-22 17:00:25 +0200

    New and create methods created

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,4 +6,18 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
   end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(required_params)
+  end
+
+  private
+
+  def required_params
+    params.requrire(:listing).permit(:name, :address, :category, :price, :image, :rating, :description, :user_id)
+  end
 end
