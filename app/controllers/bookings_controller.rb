d29139c2248aef3f9c13cdcf677f2a0a49f79d59
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-23 15:49:45 +0200

    bookings new view created and working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,14 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @listing = Listing.find(params[:listing_id])
     authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @booking.listing = Listing.find(params[:listing_id])
     authorize @booking
     if @booking.save
       redirect_to bookings_path
