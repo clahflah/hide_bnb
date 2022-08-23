93dbf289b703d6bc30fe777ba32e338b01d66a63
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-23 15:42:00 +0200

    fixed booking routes and added create and new methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :listings, only: [:index, :show, :new, :create, :destroy]
+  resources :listings do
+    resources :bookings
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
