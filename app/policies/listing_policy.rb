07184b2c47ccaa0ba806f1da631e31e00a62d156
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-23 11:55:00 +0200

    listing methods debugging

---
+++ app/policies/listing_policy.rb
@@ -0,0 +1,20 @@
+class ListingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      user.admin? ? scope.all : scope.where(user: user)
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy
+    record.user == user
+  end
+end
