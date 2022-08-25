646860f6bb9e50788bb214dc760eff9dfb182cd1
Thomsd Rice <tom@anubliss.com>
2022-08-25 22:09:00 +0200

    Added data targets, started building map on page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,6 +8,8 @@ export default class extends Controller {
     markers: Array
   }
 
+  static targets = [ "marker" ]
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue;
       this.map = new mapboxgl.Map({
@@ -23,14 +25,17 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
+      console.log(marker)
       const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       const customMarker = document.createElement("div")
-      customMarker.className = `${marker.lat}`
+      const lati = marker.lat
+      customMarker.className = `${lati}`
+      customMarker.dataset.action = "click->map#focus"
+      customMarker.dataset.mapTarget = "marker"
       customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
       customMarker.style.backgroundRepeat = "no-repeat"
       customMarker.style.color = "red"
-
       customMarker.style.width = "25px"
       customMarker.style.height = "25px"
       new mapboxgl.Marker(customMarker)
@@ -44,5 +49,12 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+  // Run this method within the addMarkersToMap method, make it require a paramter
+  // the parameter of lat will be passed to this method from the addmarler method
+  focus(event) {
+    console.log("Hello! I am updtaed again")
+    console.log()
   }
 }
